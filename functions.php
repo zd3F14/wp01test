c1da15cd58f356859e0fe22ec30da6069e0d598a
ジートライブ矢ケ崎 <zd3f14@gmail.com>
2019-03-04 15:17:07 +0900

    first commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * カスタムメニューの登録
+ */
+register_nav_menus();
+
+
+/**
+ * サイドバーの登録
+ */
+register_sidebar(
+    array(
+        'before_widget' => '<section class="widget">',
+        'after_widget' => '</nav></section>',
+        'after_title' => '</h2></nav>',
+    )
+);
